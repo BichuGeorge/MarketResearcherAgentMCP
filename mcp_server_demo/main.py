d8fd5953387ffb5ec681429a6de0fdf76e893c60
Bichu George <99749851+BichuGeorge@users.noreply.github.com>
2025-06-07 20:48:04 +0530

    Update main.py

--- mcp_server_demo/main.py
+++ mcp_server_demo/main.py
@@ -1,1 +1,17 @@
+from fastmcp import FastMCP
+from tools.mcp_swot import test_api_endpoint
+
+mcp = FastMCP("swot analysis mcp")
+
+@mcp.tool()
+def swot_analysis():
+    """
+    Run the SWOT analysis tool
+    """
+    print("Running SWOT analysis tool...")
+    return test_api_endpoint()
+    
+
+if __name__ == "__main__":
+    mcp.run()
 
